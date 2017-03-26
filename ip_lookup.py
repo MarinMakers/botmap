f0d13f799d3aa607cce72c1e610cc7fb352eea55
Sean Dow <bobthepalley@gmail.com>
2017-03-25 17:43:28 -0700

    Switching to Python 3

--- ip_lookup.py
+++ ip_lookup.py
@@ -1,4 +1,4 @@
-from urllib2 import urlopen
+from urllib.request import urlopen
 from contextlib import closing
 from pprint import pprint
 import json
@@ -6,12 +6,19 @@ import json
 # Automatically geolocate the connecting IP
 url = 'http://freegeoip.net/json/218.65.30.122'
 # try:
-with closing(urlopen(url)) as response:
-    location = json.loads(response.read())
-    pprint(location)
-    location_city = location['city']
-    location_state = location['region_name']
-    location_country = location['country_name']
+
+# with closing(urlopen(url)) as response:
+#    location = json.loads(response.read())
+#    pprint(location)
+#    location_city = location['city']
+#    location_state = location['region_name']
+#    location_country = location['country_name']
+
 #    location_zip = location['zipcode']
 # except:
 #     print("Location could not be determined automatically")
+
+ip_file = open("ips.txt", "r")
+for line in ip_file:
+	print("IP: " + line)
+
