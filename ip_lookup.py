ddec125eb641b1bd302f505b1f0399aca0b19e53
Sean Dow <bobthepalley@gmail.com>
2017-03-25 19:24:32 -0700

    ip_lookup.py writes the coordinates to ip_coordinates.txt (In short: it works)

--- ip_lookup.py
+++ ip_lookup.py
@@ -6,12 +6,9 @@ from pprint import pprint
 import json
 
 # Automatically geolocate the connecting IP
-url = 'http://freegeoip.net/json/'
+source_url = 'http://freegeoip.net/json/'
 
-
-
-
-source_filename = "test_ip.txt"
+source_filename = "ips.txt"
 destination_filename = "ip_coordinates.txt"
 
 ip_file = open(source_filename, "r")
@@ -19,11 +16,11 @@ coordinate_file = open(destination_filename, "w")
 
 for ip in ip_file:
 	ip = ip[:-1]
-	url = url + ip
+	url = source_url + ip
 	with closing(urlopen(url)) as response:
-		location = json.loads(response.reads())	
+		location = json.loads(response.read().decode("UTF-8"))	
 		location_latitude = location['latitude']
 		location_longitude = location['longitude']
-		output_string = location_latitude + "," + location_longitude
-		print(output_string)
+		output_string = str(location_latitude) + "," + str(location_longitude) + "\n"
+		coordinate_file.write(output_string)
 
