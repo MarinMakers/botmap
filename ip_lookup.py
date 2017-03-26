29de08cfa58615f46c4e5ee43de692a08900d08a
Sean Dow <bobthepalley@gmail.com>
2017-03-25 17:11:07 -0700

    Initial Commit

---
+++ ip_lookup.py
@@ -0,0 +1,17 @@
+from urllib2 import urlopen
+from contextlib import closing
+from pprint import pprint
+import json
+
+# Automatically geolocate the connecting IP
+url = 'http://freegeoip.net/json/218.65.30.122'
+# try:
+with closing(urlopen(url)) as response:
+    location = json.loads(response.read())
+    pprint(location)
+    location_city = location['city']
+    location_state = location['region_name']
+    location_country = location['country_name']
+#    location_zip = location['zipcode']
+# except:
+#     print("Location could not be determined automatically")
