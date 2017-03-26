1b127404bfdc645560cc9384d48cb63f6c61c2d3
Miles Florence <milesflo15@gmail.com>
2017-03-25 17:51:46 -0700

    Uncommenting all the things

--- ip_lookup.py
+++ ip_lookup.py
@@ -5,20 +5,19 @@ import json
 
 # Automatically geolocate the connecting IP
 url = 'http://freegeoip.net/json/218.65.30.122'
-# try:
+try:
+	with closing(urlopen(url)) as response:
+		location = json.loads(response.read())
+		pprint(location)
+		location_city = location['city']
+		location_state = location['region_name']
+		location_country = location['country_name']
 
-# with closing(urlopen(url)) as response:
-#    location = json.loads(response.read())
-#    pprint(location)
-#    location_city = location['city']
-#    location_state = location['region_name']
-#    location_country = location['country_name']
-
-#    location_zip = location['zipcode']
-# except:
-#     print("Location could not be determined automatically")
+		location_zip = location['zipcode']
+except:
+	print("Location could not be determined automatically")
 
 ip_file = open("ips.txt", "r")
 for line in ip_file:
-	print("IP: " + line)
+	print("IP: " + line, end="")
 
