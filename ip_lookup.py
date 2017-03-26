3f369c6605211a4854e62e150bc8283c8fd91c3a
Sean Dow <bobthepalley@gmail.com>
2017-03-25 18:26:20 -0700

    Printing coordinates to the console

--- ip_lookup.py
+++ ip_lookup.py
@@ -6,21 +6,33 @@ from pprint import pprint
 import json
 
 # Automatically geolocate the connecting IP
-url = 'http://freegeoip.net/json/218.65.30.122'
+url = 'http://freegeoip.net/json/'
 # try:
 
 # with closing(urlopen(url)) as response:
-#    location = json.loads(response.read())
-#    pprint(location)
-#    location_city = location['city']
-#    location_state = location['region_name']
-#    location_country = location['country_name']
+#	location = json.loads(response.read())
+#	pprint(location)
+#	location_city = location['city']
+#	location_state = location['region_name']
+#	location_country = location['country_name']
 
 #    location_zip = location['zipcode']
 # except:
 #     print("Location could not be determined automatically")
 
-ip_file = open("ips.txt", "r")
-for line in ip_file:
-	print("IP: " + line, end="")
+source_filename = "test_ip.txt"
+destination_filename = "ip_coordinates.txt"
 
+ip_file = open(source_filename, "r")
+coordinate_file = open(destination_filename, "w")
+
+for ip in ip_file:
+	ip = ip[:-1]
+	url = url + ip
+	with closing(urlopen(url)) as response:
+		location = json.loads(response.reads())	
+		location_latitude = location['latitude']
+		location_longitude = location['longitude']
+		output_string = location_latitude + "," + location_longitude
+		print(output_string)
+	
