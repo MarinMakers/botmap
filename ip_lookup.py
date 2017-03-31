3b7ad7d05080dd7231da8af38db22c76c52d0f9f
Miles Florence <milesflo15@gmail.com>
2017-03-30 17:57:39 -0700

    Improved temp CLI feedback

--- ip_lookup.py
+++ ip_lookup.py
@@ -26,12 +26,12 @@ def loadingBar(current_item, max_items):
 
 	return output_string + loading_section + " "
 
-
+# Returns bool if string is [0-255].[0-255].[0-255].[0-255] formatted
 def validIPv4(ip):
-	ip = ip.split(".")
-	if len(ip) != 4: 
+	ipArr = ip.split(".")
+	if len(ipArr) != 4: 
 		return False
-	for i in ip:
+	for i in ipArr:
 		try:
 			i = int(i)
 		except ValueError:
@@ -43,17 +43,20 @@ def validIPv4(ip):
 # Automatically geolocate the connecting IP
 source_url = 'http://freegeoip.net/json/'
 
+# Define input file with CLI argument or fallback value of ips.txt
 try:
 	source_filename = sys.argv[1]
 except IndexError:
 	source_filename = "ips.txt"
 
+# Output file
 destination_filename = "ip_coordinates.csv"
+
 
 try:
 	ip_file = open(source_filename, "r")
 except:
-	raise ValueError("Invalid source file name {0}. Exiting..".format(source_filename))
+	raise FileNotFoundError("Invalid source file name `{0}`. Exiting..".format(source_filename))
 
 coordinate_file = open(destination_filename, "w")
 
@@ -70,9 +73,9 @@ for i, ip in enumerate(ip_file):
 
 			### Something is wrong with the Loading bar feature. Revisit. For now, printing iterator on line below.
 			# print("\r" + loadingBar(i + 1, len(ip_list)), end="")
-			print(i)
+			print(i+1)
 			coordinate_file.write(output_string)
 	else:
-		print("Invalid IP: {0}".format(ip))
+		print( "Invalid IP - Line {0}: {1}".format(i+1,ip) , end="" )
 print("\nComplete!")
 
