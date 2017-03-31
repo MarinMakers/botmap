7232702a032608474740f6c1b9ffabcabece1953
Miles Florence <milesflo15@gmail.com>
2017-03-30 17:31:26 -0700

    Added input validation before api request

--- ip_lookup.py
+++ ip_lookup.py
@@ -27,6 +27,19 @@ def loadingBar(current_item, max_items):
 	return output_string + loading_section + " "
 
 
+def validIPv4(ip):
+	ip = ip.split(".")
+	if len(ip) != 4: 
+		return False
+	for i in ip:
+		try:
+			i = int(i)
+		except ValueError:
+			return False
+		if not 0<=i<=255:
+			return False 
+	return True
+
 # Automatically geolocate the connecting IP
 source_url = 'http://freegeoip.net/json/'
 
@@ -40,24 +53,26 @@ destination_filename = "ip_coordinates.csv"
 try:
 	ip_file = open(source_filename, "r")
 except:
-	raise ValueError("Invalid source file name %s. Exiting.." % source_filename)
+	raise ValueError("Invalid source file name {0}. Exiting..".format(source_filename))
 
 coordinate_file = open(destination_filename, "w")
 
 ip_list = []
 for i, ip in enumerate(ip_file):
-	ip = ip[:-1]
-	url = source_url + ip
-	with closing(urlopen(url)) as response:
-		location = json.loads(response.read().decode("UTF-8"))	
-		location_latitude = location['latitude']
-		location_longitude = location['longitude']
-		location_city = location['city']
-		output_string = str(location_latitude) + "," + str(location_longitude) + "," + location_city + "," + ip + "\n"
+	if validIPv4(ip):
+		url = source_url + ip
+		with closing(urlopen(url)) as response:
+			location = json.loads(response.read().decode("UTF-8"))	
+			location_latitude = location['latitude']
+			location_longitude = location['longitude']
+			location_city = location['city']
+			output_string = str(location_latitude) + "," + str(location_longitude) + "," + location_city + "," + ip + "\n"
 
-		### Something is wrong with the Loading bar feature. Revisit. For now, printing iterator on line below.
-		# print("\r" + loadingBar(i + 1, len(ip_list)), end="")
-		print(i)
-		coordinate_file.write(output_string)
+			### Something is wrong with the Loading bar feature. Revisit. For now, printing iterator on line below.
+			# print("\r" + loadingBar(i + 1, len(ip_list)), end="")
+			print(i)
+			coordinate_file.write(output_string)
+	else:
+		print("Invalid IP: {0}".format(ip))
 print("\nComplete!")
 
