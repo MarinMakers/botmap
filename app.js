95ed9bd7a75734e7a53fab7c0f78c7ed37e2c95d
Miles <miles.b.flo@gmail.com>
2017-07-09 17:56:02 -0700

    Restructured directory:

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ var path = require('path');
 
 var app = express()
 app.set("title","Botmap");
-app.use(express.static("public"));
 
 
-app.use((req, res, next)=> {
-	console.log(req)
-	//express.static(path.join(__dirname,'/'))
-});
+app.use("/",express.static("public"));
+
+
+// app.use((req, res, next)=> {
+// 	console.log(req)
+// 	//express.static(path.join(__dirname,'/'))
+// });
 
 
 
-app.get('/', function (req, res) {
-	var filename = "/index.html"
-	console.log(`Loading ${filename}`)
-	res.sendFile(__dirname+filename)
-})
+// app.get('/', function (req, res) {
+// 	var filename = "/public/index.html"
+// 	console.log(`Loading ${filename}`)
+// 	res.send(__dirname+filename)
+// })
 
 app.listen(3000, ()=> {
 	console.log("Server is live.")
