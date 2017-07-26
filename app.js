006353a79eddc31315415af325a2a81c4468f505
Miles <miles.b.flo@gmail.com>
2017-07-26 12:12:03 -0700

    Added frontend and backend server

--- app.js
+++ app.js
@@ -1,28 +1,73 @@
 #!/usr/bin/env node
 
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const knex = require("knex")(require("./knexfile.js"))
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const PORT = 1337;
 
 var app = express()
-app.set("title","Botmap");
 
+app.set("title","Botmap");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//app.use(methodOverride())
+
+var ipFormat = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
+
+
+//Error handler
+function errorHandler(req, res, next) {
+	var err = new Error('Not Found');
+    err.status = 404;
+    res.send("File Not Found")
+    next(err);
+}
+
+
+function validIPArr(arr) {
+	for (i in arr) {
+		if (!arr[i].match(ipFormat)) return false
+	}
+	return true
+}
+
+
+
+//Routes
+app.get("/", function (req,res,next) {
+	console.log(req.params)
+	next()
+})
+
+app.get("/map", function (req,res) {
+	console.log(`GET map with`, req.params)
+	res.sendFile(__dirname+"/public/map.html")
+	//next()
+})
+app.post("/map", function (req,res) {
+	//console.log("POST",req.body.iplist);
+
+	var ipArr = req.body.iplist.split(/[\r\n\,\ ]/).filter(function(n){ 
+		if (n==="") return false
+		return n != undefined
+	});
+
+
+
+	res.sendFile(__dirname+"/public/map.html")
+	//next()
+})
 
 app.use("/",express.static("public"));
 
 
-// app.use((req, res, next)=> {
-// 	console.log(req)
-// 	//express.static(path.join(__dirname,'/'))
-// });
-
-
-
-// app.get('/', function (req, res) {
-// 	var filename = "/public/index.html"
-// 	console.log(`Loading ${filename}`)
-// 	res.send(__dirname+filename)
-// })
-
-app.listen(3000, ()=> {
-	console.log("Server is live.")
+//This MUST be the last route
+app.use(errorHandler);
+app.listen(PORT, ()=> {
+	console.log(`Server is live at port ${PORT}`)
 });
+
+
